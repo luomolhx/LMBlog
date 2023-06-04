a173f139dd8db8ec306441bf4fe282df5d464ab3
luomolhx <2932965369@qq.com>
2023-06-04 22:43:19 +0800

    init

---
+++ luomo-blog/src/main/java/com/luomo/LuomoBlogApplication.java
@@ -0,0 +1,16 @@
+package com.luomo;
+
+/**
+ * ClassName: LuomoBlogApplication
+ * Package: com.luomo
+ * Description:
+ *
+ * @Author: luomo
+ * @Create: 2023/6/4 - 22:10
+ * @Version: v1.0
+ */
+public class LuomoBlogApplication {
+    public static void main(String[] args) {
+
+    }
+}
