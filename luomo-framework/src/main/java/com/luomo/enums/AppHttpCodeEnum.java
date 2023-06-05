8c67c6da7fb6136bc6e3aeed2b0cd981d209dc02
luomolhx <2932965369@qq.com>
2023-06-05 09:43:10 +0800

    统一响应格式

---
+++ luomo-framework/src/main/java/com/luomo/enums/AppHttpCodeEnum.java
@@ -0,0 +1,13 @@
+package com.luomo.enums;
+
+/**
+ * ClassName: AppHttpCodeEnum
+ * Package: com.luomo.enums
+ * Description:
+ *
+ * @Author: luomo
+ * @Create: 2023/6/5 - 9:41
+ * @Version: v1.0
+ */
+public enum AppHttpCodeEnum {
+}
